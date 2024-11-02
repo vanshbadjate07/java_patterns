36a382e6a1555b79af9074894a0b16f70e0307ad
Vansh Badjate <128991304+vanshbadjate07@users.noreply.github.com>
2024-11-02 17:07:22 +0530

    Add files via upload

---
+++ Codes/square_star.java
@@ -0,0 +1,22 @@
+/*
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+ */
+
+class square_star{
+    public static void main(String args[]){
+        int rows=5;
+        int cols=5;
+
+        for(int i=0;i<=rows;i++){
+            for(int j=0;j<=cols;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
